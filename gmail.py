ef5d33c3ed68a0ce21f44df65508b5d2075df40e
IvanVnucec <vnucec.ivan@gmail.com>
2021-04-03 01:54:50 +0200

    remove login method enter credentials on init login in init method

--- gmail.py
+++ gmail.py
@@ -7,24 +7,24 @@ class Gmail:
     SSL_PORT = 465
     SMTP_GMAIL = 'smtp.gmail.com'
 
-    def __init__(self) -> None:
-        self.login_successfully = False
-
-    def login(self, email, password):
+    def __init__(self, email, password) -> None:
+        self.loginSuccessfull = False
         self.email = email
-        self.__password = password
+
+        context = ssl.create_default_context()
+        self.server = smtplib.SMTP_SSL(
+            self.SMTP_GMAIL, self.SSL_PORT, context=context)
+        try:
+            self.server.login(self.email, password)
+        except smtplib.SMTPAuthenticationError as error:
+            exit('ERROR: Wrong username or password.')
+        else:
+            self.loginSuccessfull = True
 
     def send(self, message, receiver_email):
-        context = ssl.create_default_context()
-
-        with smtplib.SMTP_SSL(self.SMTP_GMAIL, self.SSL_PORT, context=context) as server:
-            try:
-                server.login(self.email, self.__password)
-
-            except smtplib.SMTPAuthenticationError as error:
-                print('ERROR: Wrong username or password.')
-
-            else:
-                email_msg = message_from_string(message)
-                server.send_message(
-                    email_msg, self.email, receiver_email)
+        if self.loginSuccessfull:
+            email_msg = message_from_string(message)
+            self.server.send_message(
+                email_msg, self.email, receiver_email)
+        else:
+            exit('ERROR: To send en email, you first need to login.')
