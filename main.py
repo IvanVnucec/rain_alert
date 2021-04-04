1831abe75129142d115be598482687a06212f923
Ivan Vnučec <50591550+IvanVnucec@users.noreply.github.com>
2021-04-04 22:04:37 +0200

    code refactor CI update

--- main.py
+++ main.py
@@ -1,26 +1,57 @@
 import utils
 from gmail import Gmail
 from open_weather import OpenWeather
+from datetime import datetime
+import pytz
 
-sender, password, openWeatherApiKey = utils.load_credentials()
 
-gmail = Gmail(sender, password)
-openWeather = OpenWeather(openWeatherApiKey)
+__DAY_START_HOUR = 6
+__DAY_END_HOUR = 23
+__RAIN_PROB_TRESH = 0.5
+__ZAGREB_TIMEZONE = 'Europe/Zagreb'
 
-receivers = utils.load_emails()
 
-for receiver in receivers:
-    # TODO: implement will_rain_today function here.
-    # openWeather object should only get data.
-    rainToday, rainStart = openWeather.will_rain_today()
+def will_rain_today():
+    rainToday = False
+    rainStartHour = None
 
-    if rainToday:
-        # TODO: Construct message so it contains probabilities
-        # by hour
-        subject = 'Rain today in Zagreb'
-        msg = f'Rain today at {rainStart}.'
+    forecastByHour = openWeather.get_forecast_by_hour()
 
-        gmail.send(receiver, subject, msg)
+    localTimeNow = datetime.now(pytz.timezone(__ZAGREB_TIMEZONE))
 
-    else:
-        print('INFO: No rain today in Zagreb.')
+    for forecast in forecastByHour:
+        forecastTime = forecast['t']
+        probability = forecast['p']
+        sameDayForecast = localTimeNow.day == forecastTime.day
+        inTimeRange = forecastTime.hour >= __DAY_START_HOUR and forecastTime.hour <= __DAY_END_HOUR
+        highRainProbability = probability >= __RAIN_PROB_TRESH
+
+        if sameDayForecast and inTimeRange and highRainProbability:
+            rainToday = True
+            rainStartHour = forecastTime.hour
+            break
+
+    return rainToday, rainStartHour
+
+
+if __name__ == "__main__":
+    sender, password, openWeatherApiKey = utils.load_credentials()
+
+    gmail = Gmail(sender, password)
+    openWeather = OpenWeather(openWeatherApiKey)
+
+    receivers = utils.load_emails()
+
+    for receiver in receivers:
+        rainToday, rainStart = will_rain_today()
+
+        if rainToday:
+            # TODO: Construct message so it contains probabilities
+            # by hour
+            subject = 'Rain today in Zagreb'
+            msg = f'Rain today at {rainStart}.'
+
+            gmail.send(receiver, subject, msg)
+
+        else:
+            print('INFO: No rain today in Zagreb.')
--- open_weather.py
+++ open_weather.py
@@ -4,36 +4,32 @@ from datetime import datetime
 import pytz
 
 
-__DAY_START_HOUR = 6
-__DAY_END_HOUR = 23
-__RAIN_PROB_TRESH = 0.5
 __ZAGREB_LAT = 45.815399
 __ZAGREB_LON = 15.966568
-__ZAGREB_TIMEZONE = 'Europe/Zagreb'
 
 
 class OpenWeather:
     def __init__(self, apiKey) -> None:
         self.__apiKey = apiKey
 
-    def _get_data_from_api(self, apiUrl):
-        try:
-            response = requests.get(apiUrl)
-        except:
-            exit('ERROR: OpenWeather API request failed.')
-
-        return response.json()
-
-    def _get_forecast_by_hour(self):
-        forecastByHour = []
-
-        apiUrl = 'http://api.openweathermap.org/data/2.5/onecall?'\
+    def _get_data_from_api(self):
+        API_URL = 'http://api.openweathermap.org/data/2.5/onecall?'\
             f'&lat={__ZAGREB_LAT}'\
             f'&lon={__ZAGREB_LON}'\
             '&exclude=current,minutely,daily,alerts&units=metric'\
             f'&appid={self.__apiKey}'
 
-        data = self._get_data_from_api(apiUrl)
+        try:
+            response = requests.get(API_URL)
+        except:
+            exit('ERROR: OpenWeather API request failed.')
+
+        return response.json()
+
+    def get_forecast_by_hour(self):
+        forecastByHour = []
+
+        data = self._get_data_from_api()
 
         timezoneOffset = data['timezone_offset']
         byHours = data['hourly']
@@ -48,25 +44,3 @@ class OpenWeather:
             forecastByHour.append(rainData)
 
         return forecastByHour
-
-    def will_rain_today(self):
-        rainToday = False
-        rainStartHour = None
-
-        forecastByHour = self._get_forecast_by_hour()
-        
-        timeNow = datetime.now(pytz.timezone(__ZAGREB_TIMEZONE))
-
-        for forecast in forecastByHour:
-            time = forecast['t']
-            probability = forecast ['p']
-            sameDayForecast = timeNow.day == time.day
-            inTimeRange = time.hour >= __DAY_START_HOUR and time.hour <= __DAY_END_HOUR
-            highRainProbability = probability >= __RAIN_PROB_TRESH
-
-            if sameDayForecast and inTimeRange and highRainProbability:
-                rainToday = True
-                rainStartHour = hour['t'].hour
-                break
-
-        return rainToday, rainStartHour
