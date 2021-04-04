d9e5414aca5704c4c0e39b1ea0eb8f2392b2f060
Ivan Vnučec <50591550+IvanVnucec@users.noreply.github.com>
2021-04-04 13:13:05 +0200

    Update open_weather.py

--- open_weather.py
+++ open_weather.py
@@ -4,14 +4,15 @@ from datetime import datetime
 import pytz
 
 
-class OpenWeather:
-    __DAY_START_HOUR = 6
-    __DAY_END_HOUR = 23
-    __RAIN_PROB_TRESH = 0.5
-    __ZAGREB_LAT = 45.815399
-    __ZAGREB_LON = 15.966568
-    __ZAGREB_TIMEZONE = 'Europe/Zagreb'
+__DAY_START_HOUR = 6
+__DAY_END_HOUR = 23
+__RAIN_PROB_TRESH = 0.5
+__ZAGREB_LAT = 45.815399
+__ZAGREB_LON = 15.966568
+__ZAGREB_TIMEZONE = 'Europe/Zagreb'
 
+
+class OpenWeather:
     def __init__(self, apiKey) -> None:
         self.__apiKey = apiKey
 
@@ -23,12 +24,12 @@ class OpenWeather:
 
         return response.json()
 
-    def _get_forecast_by_hour(self, latitude, longitude):
+    def _get_forecast_by_hour(self):
         forecastByHour = []
 
         apiUrl = 'http://api.openweathermap.org/data/2.5/onecall?'\
-            f'&lat={latitude}'\
-            f'&lon={longitude}'\
+            f'&lat={__ZAGREB_LAT}'\
+            f'&lon={__ZAGREB_LON}'\
             '&exclude=current,minutely,daily,alerts&units=metric'\
             f'&appid={self.__apiKey}'
 
@@ -52,16 +53,18 @@ class OpenWeather:
         rainToday = False
         rainStartHour = None
 
-        forecastByHour = self._get_forecast_by_hour(self.__ZAGREB_LAT, self.__ZAGREB_LON)
+        forecastByHour = self._get_forecast_by_hour()
         
-        timeNow = datetime.now(pytz.timezone(self.__ZAGREB_TIMEZONE))
+        timeNow = datetime.now(pytz.timezone(__ZAGREB_TIMEZONE))
 
-        for hour in forecastByHour:
-            sameDayForecast = timeNow.day == hour['t'].day
-            inDayTimeRange = hour['t'].hour >= self.__DAY_START_HOUR and hour['t'].hour <= self.__DAY_END_HOUR
-            highRainProbability = hour['p'] >= self.__RAIN_PROB_TRESH
+        for forecast in forecastByHour:
+            time = forecast['t']
+            probability = forecast ['p']
+            sameDayForecast = timeNow.day == time.day
+            inTimeRange = time.hour >= __DAY_START_HOUR and time.hour <= __DAY_END_HOUR
+            highRainProbability = probability >= __RAIN_PROB_TRESH
 
-            if sameDayForecast and inDayTimeRange and highRainProbability:
+            if sameDayForecast and inTimeRange and highRainProbability:
                 rainToday = True
                 rainStartHour = hour['t'].hour
                 break
