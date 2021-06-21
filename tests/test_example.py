b912c7c2b5a40894828848f2dde738dece07281a
IvanVnucec <vnucec.ivan@gmail.com>
2021-06-21 22:08:27 +0200

    add unit testing

---
+++ tests/test_example.py
@@ -0,0 +1,11 @@
+"""
+This file serves as test template!
+"""
+
+import unittest
+
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(3+2+1, 6, "Message if fail")
