a13fdbf452f92470d53def4eb1593ebec8c4c8ab
Ivan Vnucec <vnucec.ivan@gmail.com>
2024-10-04 20:00:46 +0200

    simplify and update weather api

--- README.md
+++ README.md
@@ -1,113 +1,22 @@
+# rain_alert
+
 [![App running](https://github.com/IvanVnucec/rain_alert/actions/workflows/weather_check.yml/badge.svg?branch=master&event=schedule)](https://github.com/IvanVnucec/rain_alert/actions/workflows/weather_check.yml)
 
-# rain_alert
-You will not forget your umbrella anymore. :umbrella:
+You will not forget your :umbrella: anymore.
 
 ## About
-Check every morning at 5 AM local time if it will be raining that day, if yes 
-send an email with forecast message like this:
-<html>
-  <table>
-    <tr>
-        <th>Hour [h]</th>
-        <th>Probability [%]</th>
-    </tr>
-    <tr>
-        <td>6</td>
-        <td id="CELL0">0</td>
-    </tr>
-    <tr>
-        <td>7</td>
-        <td id="CELL1">0</td>
-    </tr>
-    <tr>
-        <td>8</td>
-        <td id="CELL2">0</td>
-    </tr>
-    <tr>
-        <td>9</td>
-        <td id="CELL3">0</td>
-    </tr>
-    <tr>
-        <td>10</td>
-        <td id="CELL4">10</td>
-    </tr>
-    <tr>
-        <td>11</td>
-        <td id="CELL5">22</td>
-    </tr>
-    <tr>
-        <td>12</td>
-        <td id="CELL6">35</td>
-    </tr>
-    <tr>
-        <td>13</td>
-        <td id="CELL7">60</td>
-    </tr>
-    <tr>
-        <td>14</td>
-        <td id="CELL8">86</td>
-    </tr>
-    <tr>
-        <td>15</td>
-        <td id="CELL9">100</td>
-    </tr>
-    <tr>
-        <td>16</td>
-        <td id="CELL10">100</td>
-    </tr>
-    <tr>
-        <td>17</td>
-        <td id="CELL11">100</td>
-    </tr>
-    <tr>
-        <td>18</td>
-        <td id="CELL12">100</td>
-    </tr>
-    <tr>
-        <td>19</td>
-        <td id="CELL13">72</td>
-    </tr>
-    <tr>
-        <td>20</td>
-        <td id="CELL14">36</td>
-    </tr>
-    <tr>
-        <td>21</td>
-        <td id="CELL15">5</td>
-    </tr>
-    <tr>
-        <td>22</td>
-        <td id="CELL16">0</td>
-    </tr>
-    <tr>
-        <td>23</td>
-        <td id="CELL17">0</td>
-    </tr>
-  </table>
-</html>
+
+Check the weather at 5AM Zagreb time and send an email if it will rain today.
 
 ## Get started
-0. Create Gmail account and enable the Less secure app access and also create an OpenWeather API Key.
-1. Create `credentials/credentials.yaml` file and put Gmail and OpenWeather credentials from step 0.
-```
-senderEmail: <sender Gmail email>
-senderPassword: <Gmail email password>
-openWeatherApiKey: <OpenWeather API key>
-```
-2. Create `credentials/receivers.txt` file put in email subscribers. For example:
-```
-example1@email.com, Zagreb
-example2@email.com, Berlin
-example3@email.com, Milwaukee
-example4@email.com, Mobile Alabama
-example5@email.com, Nashville Tennessee
-example6@email.com, Nashville Indiana
-```
-3. Run the app using Makefile as `make run`
-4. (Recommended) You can schedule the script to run on GitHub servers like we did in 
-[our GitHub Actions CI workflow](https://github.com/IvanVnucec/rain_alert/blob/master/.github/workflows/weather_check.yml). 
-See the [Instructions](./.github/workflows/README.md) for more info.
+
+1. Create Gmail account and enable the Less secure app access.
+2. Add the following environment variables to GitHub actions:
+    - `SENDER_EMAIL` - sender Gmail email
+    - `SENDER_PASSWORD` - Gmail email password
+    - `RECEIVERS` - list of email addresses to send the alert to
+3. Run the GitHub Action workflow manually to check if everything is working.
 
 ## License
+
 [MIT](LICENSE.md)
--- main.py
+++ main.py
@@ -1,70 +1,94 @@
-from datetime import time
-from utils import error, get_email_credentials, get_github_actions_url, get_receivers, debug
-from gmail import Gmail
-from forecast import Forecast
-from location import Location
-from exec_tracker import ExecTracker
+import os
 
-SEND_EMAIL_HOUR = 5  # AM local time
+# Get hourly precipitation probability for Zagreb
+import urllib.request
+import json
+ZAGREB_WEATHER_API = "https://api.open-meteo.com/v1/forecast?latitude=45.8144&longitude=15.978&hourly=precipitation_probability&timezone=Europe%2FBerlin&forecast_days=1"
+with urllib.request.urlopen(ZAGREB_WEATHER_API) as response:
+    assert response.status == 200
+    data = json.loads(response.read())
 
+from datetime import datetime
+data_hourly = data["hourly"]
+precipitation = [(datetime.fromisoformat(time), int(prob)) for time, prob in zip(data_hourly["time"], data_hourly["precipitation_probability"])]
 
-def send_forecast_message(gmail, receiver, message):
-    subject, msgPlain, msgHtml = message
-    gmail.send(receiver, subject, msgPlain, msgHtml)
+# If there is a high probability of rain, send an email
+if any(prob >= 0.5 for _, prob in precipitation):
+    import smtplib
+    import ssl
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+    SSL_PORT = 465
+    SMTP_GMAIL = 'smtp.gmail.com'
+    context = ssl.create_default_context()
+    server = smtplib.SMTP_SSL(SMTP_GMAIL, SSL_PORT, context=context)
+    sender_email, password = os.getenv('SENDER_EMAIL'), os.getenv('SENDER_PASSWORD')
+    assert sender_email is not None and password is not None
+    server.login(sender_email, password)
 
+    # get first high probability of rain
+    hour_start = [time for time, prob in precipitation if prob >= 0.5][0]
+    # Construct message subject and HTML content
+    subject = f"Padaline u Zagrebu od {hour_start.strftime('%H:%M')}h"
+    content = """<!DOCTYPE html>
+<html>
+    <head>
+        <style type="text/css">
+            table {
+                font-family: arial, sans-serif;
+                border-collapse: collapse;
+                width: 40%;
+            }
+            table td,
+            th {
+                border: 1px solid #afafaf;
+                text-align: center;
+                padding: 8px;
+            }"""
+    for id,forecast in enumerate(precipitation):
+        alpha = round(forecast[1] * 0.6, 2)
+        bColor = f'hsla(240, 100%, 50%, {alpha})'
+        content += f"""
+            table td#CELL{id} {{
+                background-color: {bColor};
+                color: black;
+            }}"""
 
-def main():
-    debug(get_github_actions_url())
-    sender, password = get_email_credentials()
-    receivers = get_receivers()
-    if len(receivers) == 0: error('No receivers imported.')
+    content += f"""
+        </style>
+    </head>
+    <body>
+        <h2>Zagreb forecast</h2>
+        <table>
+            <tr>
+                <th>Hour [h]</th>
+                <th>Probability [%]</th>
+            </tr>"""
+    for id,forecast in enumerate(precipitation):
+        hourStr = str(forecast[0].hour)
+        probStr = str(forecast[1])
+        content += f"""
+            <tr>
+                <td>{hourStr}</td>
+                <td id="CELL{id}">{probStr}</td>
+            </tr>
+        """
 
-    gmail = Gmail(sender, password)
-    
-    # use password from email to encrypt exec tracker file
-    track = ExecTracker(password)
-
-    for locationName, emails in receivers.items():
-        location = Location(locationName)
-
-        executed_today = track.script_executed_today(location)
-        time_to_send_email = location.get_local_time().hour >= SEND_EMAIL_HOUR
-
-        if executed_today:
-            debug('Script already executed today, no need to fetch forecast.')
-        if not time_to_send_email:
-            debug(f'Its not time to send an email. Waiting for {SEND_EMAIL_HOUR} A.M. local time.')
-
-        if not executed_today and time_to_send_email:
-            """ mark execution time only when time to send en email because 
-            we don't want to exceed the number of free OpenWeather API calls """ 
-            debug("Script not executed today and its time to send en email.")
-            debug('Mark executed time.')
-            track.mark_exec_time(location)
-
-            forecast = Forecast(location)
-
-            if forecast.rain_today():
-                debug('It will rain today.')
-                message = forecast.get_forecast_message()
-
-                for email in emails:
-                    debug('Sending email message.')
-                    send_forecast_message(gmail, email, message)
-            else:
-                debug('It will not rain today.')
-
-    debug('Closing execution timetable.')
-    track.close()
-    debug('Checking if timetable is modified')
-    if track.is_timetable_modified():
-        debug('Timetable modified')
-        track.add_and_commit_timetable()
-    else:
-        debug('Timetable not modified')
-
-    debug("Application finished.")
-
-
-if __name__ == "__main__":
-    main()
+    content += """</table>"""
+    action_url = os.getenv('ACTION_URL') or "Unknown"
+    content += f"""
+        <p><a href="{action_url}">GitHub Action Run</a></p>
+    </body>
+</html>
+"""
+    print(content)
+    receivers = os.getenv('RECEIVERS')
+    assert receivers is not None
+    receivers = receivers.split('\n')
+    for receiver in receivers:
+        message = MIMEMultipart("alternative")
+        message['From'] = sender_email
+        message['To'] = receiver
+        message['Subject'] = subject
+        message.attach(MIMEText(content, "html"))
+        server.send_message(message, sender_email, receiver)
